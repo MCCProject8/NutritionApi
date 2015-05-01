72d039b49f219da88c65e1cd1a540bb451430dde
Brandon VanderMey <brandon.vandermey@gmail.com>
2015-05-01 00:48:41 -0400

    webpanel file

---
+++ src/WebPanel.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+
+@SuppressWarnings("serial")
+public class WebPanel extends JPanel
+{
+	public WebPanel()
+	{
+		setBackground(Color.RED);
+	}
+}
