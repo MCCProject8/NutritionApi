c85fa77f446c2a4625f43f0320bc43ce84cc54a2
KyletheWolff <kyle.a.wolff87@gmail.com>
2015-05-01 01:29:00 -0400

    YAY!!!!!!!

--- src/WebPanel.java
+++ src/WebPanel.java
@@ -1,4 +1,3 @@
-import java.awt.Color;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -11,6 +10,18 @@ public class WebPanel
 {
 	public static void initFX(final JFXPanel fxPanel)
 	{
+		CreateNutritionLabel htmldoc = new CreateNutritionLabel();
+		htmldoc.startHtmlDocument();
+		htmldoc.addTitle("Pizza");
+		htmldoc.addServingSize("1", "90g");
+		htmldoc.addLargeBar();
+		htmldoc.addAmountPerServing();
+		htmldoc.addMediumBar();
+		htmldoc.addLine("Total Fat", "90g");
+		htmldoc.addLine("Bananas", "90g", "30");
+		htmldoc.addIndentedLine("Sat", "4g");
+		htmldoc.addIndentedLine("Trans Fat", "7g", "5");
+		htmldoc.endHtmlDocument();
 		
 		Group group = new Group();
         Scene scene = new Scene(group);
@@ -22,7 +33,10 @@ public class WebPanel
         webView.setMinSize(300, 300);
         webView.setMaxSize(500, 500);
 		WebEngine webEngine = webView.getEngine();
-		webEngine.load("http://www.google.com/");
+		
+		String x = String.valueOf(htmldoc.getHtml());
+		
+		webEngine.loadContent(x);
 		
 	}
 }
